0f3e69e047520325fd45de845d1034fd61b2489d
Kamel <kamleelsayed740@gmail.com>
2023-03-17 14:40:57 -0700

    kamel

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - print alphapet from a to z and A to Z
+*
+*Return 0
+*/
+int main(void)
+{int i;
+	for (i = 97; i <= 122; i++)
+{
+	putchar(i);
+}
+	for (i = 65; i <= 90; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+	return (0);
+}

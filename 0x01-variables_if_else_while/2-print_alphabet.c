5a927025901047c65a5b09629a3026016aaef2cd
Kamel <kamleelsayed740@gmail.com>
2023-03-17 14:21:47 -0700

    ola

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - print alphapet from a to z
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{int i;
+	for (i = 97; i <= 122; i++)
+	putchar(i);
+	return (0);
+}

65127b0d1d885a9d3ac2fc603cfcca0dd65f83e9
Kamel <kamleelsayed740@gmail.com>
2023-03-21 18:02:33 -0700

    done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - to print the last digit
+ * Return: the last digit
+ * @i: is the numper that we want the last digit from it
+ * written by kamel
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+
+}

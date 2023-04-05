a3a17bf49c4cf168e9eaccac6ae2026c33ba7e4e
Kamel <kamleelsayed740@gmail.com>
2023-04-05 10:50:17 -0700

    done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * main -cheack
+ * _puts_recursion - fuction pritnt strint with anew line in the end
+ * @s: input type string
  * Return :0
  * made by kamle
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_putchar (*s);
-		_puts_recursion (s + 1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar ('\n');

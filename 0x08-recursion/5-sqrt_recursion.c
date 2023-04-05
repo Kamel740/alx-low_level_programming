494d217fd9311418064605a32e1d1b2ab53653af
Kamel <kamleelsayed740@gmail.com>
2023-04-05 13:07:32 -0700

    done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int kamel(int n, int i);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root of
@@ -13,11 +15,11 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	return (s(n, 0));
+	return (kamel(n, 0));
 }
 
 /**
- * s - recurses to find the natural
+ * kamel - recurses to find the natural
  * square root of a number
  * @n: number to calculate the sqaure root of
  * @i: iterator
@@ -25,11 +27,11 @@ int _sqrt_recursion(int n)
  * Return: the resulting square root
  */
 
-int s(int n, int i)
+int kamel(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (s(n, i + 1));
+	return (kamel(n, i + 1));
 }

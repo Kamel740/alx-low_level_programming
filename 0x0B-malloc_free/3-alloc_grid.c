d84d00d4ef8b76f5906363a22d33fac7b45a762c
Kamel <kamleelsayed740@gmail.com>
2023-04-11 09:25:30 -0700

    done

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **kamel;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	kamel = malloc(sizeof(int *) * height);
+	if (kamel == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < height; x++)
+	{
+		kamel[x] = malloc(sizeof(int)*width);
+		if (kamel[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(kamel[x])
+			free(kamel)
+			return (NULL);
+		}
+	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+
+			mee[x][y] = 0;
+	}
+	return (mee);
+}

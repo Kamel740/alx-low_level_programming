212d22f4ac662f52491a9580aef626b8092952b4
Kamel <kamleelsayed740@gmail.com>
2023-04-19 16:58:34 -0700

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * print_name - used to print aname
  * @name: the name
@@ -7,9 +9,9 @@
  * the name
  * Return:void
  */
-void print_name(char *name, void(*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	f(name);
+	(*f)(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-void print_name(char *name,void(*f)(char *);
+void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *arary, size_t size,void(*actoin)(int));
 int int_index(int *array, int size, int (*cmp)(int));

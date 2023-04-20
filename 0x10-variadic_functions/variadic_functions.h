b353581fff65d00726f7ac1f6529f70c6a304339
Kamel <kamleelsayed740@gmail.com>
2023-04-20 14:07:33 -0700

    done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,13 @@
-#ifndef _VARIADIC_H_
-#define _VARIADIC_H_
+#ifndef VARIADIC_H_
+#define VARIADIC_H_
+
+#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+
+typedef struct validTypes
+{
+	char *valid;
+	void (*f)();
+} v_types;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);

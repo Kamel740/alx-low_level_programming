0c3d656ca2010cdc364887452bb58afe71d2bb26
Kamel <kamleelsayed740@gmail.com>
2023-04-19 17:06:27 -0700

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,9 @@
 #include "function_pointers.h"
 /**
- * print_name - used to print aname
- * @name: the name
- * @f: is a pointer to function
- * that used to print the name or manupliat
- * the name
- * Return:void
- */
+  * print_name - prints a name
+  * @name: input string
+  * @f: function pointer casted to char pointer
+  */
 void print_name(char *name, void (*f)(char *))
 {
 	(*f)(name);

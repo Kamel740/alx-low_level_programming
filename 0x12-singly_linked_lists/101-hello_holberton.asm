bff230090b823431ad02b33b6c1a819835175c8d
kamel740 <kamelelsayed740@gmail.com>
2023-04-27 19:36:31 -0700

    done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,18 @@
 section .data
-message db "Hello, Holberton" , 0Ah;
-db "%s", 0;
+
+message db "Hello, Holberton" , 0Ah
+format db "%s", 0
+
 section .text
 
 	global _start
 _start:
-sub rsp, 8;
-mov rdi, format;
-mov rsi, message;
-xor eax, eax;
-call printf;
-add rsp, 8;
-xor eax, eax;
-mov ebx, eax;
-mov eax, 60;
+sub rsp, 8
+mov rdi, format
+mov rsi, message
+xor eax, eax
+call printf
+add rsp, 8
+xor eax, eax
+mov ebx, eax
+mov eax, 60
